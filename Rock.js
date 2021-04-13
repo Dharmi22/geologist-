0723228d7a9b37e38e9a71022bd96e21bf69f593
Dharmi22 <77969096+Dharmi22@users.noreply.github.com>
2021-04-13 22:50:13 +0530

    Add files via upload

---
+++ Rock.js
@@ -0,0 +1,36 @@
+class Rock{
+	constructor(x,y,width,height)
+	{
+	// assign options to the rock ball
+	var options = {
+		restitution:0.9,
+		friction:1,
+		density:5
+
+	}
+	this.body=Bodies.rectangle(x,y,width,height,options)
+		this.width=width;
+		this.height=height;
+		
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			var pos = this.body.position;		
+            var angle = this.body.angle;
+			push();
+			translate(pos.x, pos.y);
+            rotate(angle)
+			rectMode(CENTER)
+			strokeWeight(4);
+			stroke("black");
+			//draw the rectangle here to display the rubber ball
+			fill("yellow");
+			rect(0,0,this.width,this.height); 
+			
+
+			pop();
+	}
+
+}
---
+++ sketch.js
@@ -0,0 +1,40 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer;
+var rock  ;
+var rubber;
+var plane;
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20);
+    hammer = new Hammer(10,100);
+    rock = new Rock(650,300,100,100);
+    rubber = new Rubber(900,400,60);
+
+  
+
+
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+
+    plane.display();
+    hammer.display();
+    rock.display();
+    rubber.display();
+
+
+
+    
+ 
+}
